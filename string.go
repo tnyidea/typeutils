8aa8d5c58f2bc2ba76d6b30c3a1d3d1d70550a74
tnyidea Ventures <tnyidea@gmail.com>
2020-11-12 00:45:57 -0500

    Added TruncateString

--- string.go
+++ string.go
@@ -1,8 +1,17 @@
 package typeutils
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func StringPtr(s string) *string {
 	if s == "" {
 		return nil
 	}
 	return &s
+}
+
+func TruncateString(s string, length int, suffix string) string {
+	return fmt.Sprintf("%."+strconv.Itoa(length-len(suffix))+"s"+suffix, s)
 }
