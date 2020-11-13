0d8d39e08b4f59b982536fe5aaa84f234b36a6eb
tnyidea Ventures <tnyidea@gmail.com>
2020-11-13 10:46:30 -0500

    Fixed TruncateString for default length return

--- string.go
+++ string.go
@@ -13,5 +13,8 @@ func StringPtr(s string) *string {
 }
 
 func TruncateString(s string, length int, suffix string) string {
+	if len(s) <= length {
+		return s
+	}
 	return fmt.Sprintf("%."+strconv.Itoa(length-len(suffix))+"s"+suffix, s)
 }
